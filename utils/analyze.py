4f1f65bc4fb9c2d0c93587d6c017e8abc1964464
SleepWalker <dev@udf.su>
2024-06-30 20:24:40 +0300

    Add more metrics and graphs in analyze package reports

--- utils/analyze.py
+++ utils/analyze.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import (
     classification_report as sklearn_report,
     roc_auc_score,
@@ -6,6 +10,8 @@ from sklearn.metrics import (
     precision_score,
     recall_score,
     f1_score,
+    confusion_matrix,
+    RocCurveDisplay,
 )
 
 
@@ -55,10 +61,11 @@ def classification_report(
 
 
 def multiclass_report(
-    y_true_tr,
-    y_proba_tr,
-    y_true_val=None,
-    y_proba_val=None,
+    y_true_tr: np.ndarray,
+    y_proba_tr: np.ndarray,
+    labels: list,
+    y_true_val: np.ndarray = None,
+    y_proba_val: np.ndarray = None,
     report=False,
 ):
     def round(input: float) -> float:
@@ -121,10 +128,234 @@ def multiclass_report(
             print("{:<15} {:<10}".format(metrics, value))
 
     if report:
+        y_tr = np.argmax(y_proba_tr, axis=1)
+
         print("\n")
         print("Train:")
-        print(sklearn_report(y_true_tr, np.argmax(y_proba_tr, axis=1)))
+        print(sklearn_report(y_true_tr, y_tr))
+        plot_confusion_matrix(
+            y_true_tr,
+            y_tr,
+            labels=labels,
+            subtitle="Recall. Train dataset",
+            normalize="true",
+        )
+        plot_confusion_matrix(
+            y_true_tr,
+            y_tr,
+            labels=labels,
+            subtitle="Precision. Train dataset",
+            normalize="pred",
+        )
+        plot_multiclass_roc_curve(
+            y_true_tr,
+            y_proba_tr,
+            labels=labels,
+            subtitle="Train dataset",
+        )
+        plot_scoring_distribution(
+            y_true_tr,
+            y_proba_tr,
+            labels=labels,
+            subtitle="Train dataset",
+        )
 
         if not y_true_val.empty:
+            y_val = np.argmax(y_proba_val, axis=1)
+
             print("Test:")
-            print(sklearn_report(y_true_val, np.argmax(y_proba_val, axis=1)))
+            print(sklearn_report(y_true_val, y_val))
+            plot_confusion_matrix(
+                y_true_val,
+                y_val,
+                labels=labels,
+                subtitle="Recall. Test dataset",
+                normalize="true",
+            )
+            plot_confusion_matrix(
+                y_true_val,
+                y_val,
+                labels=labels,
+                subtitle="Precision. Test dataset",
+                normalize="pred",
+            )
+            plot_multiclass_roc_curve(
+                y_true_val,
+                y_proba_val,
+                labels=labels,
+                subtitle="Test dataset",
+            )
+            plot_multiclass_roc_curve_train_vs_test(
+                y_true_tr=y_true_tr,
+                y_proba_tr=y_proba_tr,
+                y_true_val=y_true_val,
+                y_proba_val=y_proba_val,
+                labels=labels,
+            )
+            plot_scoring_distribution(
+                y_true_val,
+                y_proba_val,
+                labels=labels,
+                subtitle="Test dataset",
+            )
+
+
+def plot_confusion_matrix(
+    y_true: np.ndarray,
+    y_pred: np.ndarray,
+    labels: list,
+    subtitle: str = None,
+    normalize=None,
+):
+    data = confusion_matrix(y_true, y_pred, normalize=normalize)
+
+    title = "Confusion matrix"
+
+    if subtitle:
+        title = f"{title} ({subtitle})"
+
+    fig = px.imshow(
+        pd.DataFrame(data, index=labels, columns=labels),
+        text_auto=".2f",
+        title=title,
+        height=700,
+        width=700,
+    )
+    fig.update_xaxes(
+        showgrid=False,
+        title_text="predicted label",
+    ).update_yaxes(
+        showgrid=False,
+        title_text="actual label",
+    )
+    fig.show()
+
+
+def plot_multiclass_roc_curve(
+    y_true: np.ndarray,
+    y_pred_proba: np.ndarray,
+    labels: list,
+    subtitle: str = None,
+):
+    num_classes = y_pred_proba.shape[1]
+
+    assert num_classes == len(
+        labels
+    ), f"y_pred_proba size {num_classes} does not match labels count {len(labels)}"
+
+    label_binarizer = LabelBinarizer().fit(np.arange(num_classes))
+    y_true_ohe = label_binarizer.transform(y_true)
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    RocCurveDisplay.from_predictions(
+        y_true_ohe.ravel(),
+        y_pred_proba.ravel(),
+        name="micro-average ROC curve",
+        color="deeppink",
+        linestyle=":",
+        linewidth=4,
+        ax=ax,
+    )
+
+    for class_id, label in enumerate(labels):
+        RocCurveDisplay.from_predictions(
+            y_true_ohe[:, class_id],
+            y_pred_proba[:, class_id],
+            name=f"ROC for {label}",
+            ax=ax,
+            # plot only for last class because it's the same for all
+            plot_chance_level=(class_id == len(labels) - 1),
+        )
+
+    title = "One-vs-Rest Receiver Operating Characteristic (OVR ROC)"
+
+    if subtitle:
+        title = f"{title}\n{subtitle}"
+
+    _ = ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title=title,
+    )
+
+
+def plot_multiclass_roc_curve_train_vs_test(
+    y_true_tr: np.ndarray,
+    y_proba_tr: np.ndarray,
+    y_true_val: np.ndarray,
+    y_proba_val: np.ndarray,
+    labels: list,
+):
+    num_classes = y_proba_tr.shape[1]
+
+    assert num_classes == len(
+        labels
+    ), f"y_pred_proba size {num_classes} does not match labels count {len(labels)}"
+
+    label_binarizer = LabelBinarizer().fit(np.arange(num_classes))
+    y_true_tr_ohe = label_binarizer.transform(y_true_tr)
+    y_true_val_ohe = label_binarizer.transform(y_true_val)
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    RocCurveDisplay.from_predictions(
+        y_true_tr_ohe.ravel(),
+        y_proba_tr.ravel(),
+        name="ROC for Train",
+        ax=ax,
+    )
+
+    RocCurveDisplay.from_predictions(
+        y_true_val_ohe.ravel(),
+        y_proba_val.ravel(),
+        name="ROC for Test",
+        ax=ax,
+        plot_chance_level=True,
+    )
+
+    _ = ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title="Train vs test ROC OVR",
+    )
+
+
+def plot_scoring_distribution(
+    y_true: np.ndarray,
+    y_pred_proba: np.ndarray,
+    labels: list,
+    subtitle: str = None,
+):
+    num_classes = y_pred_proba.shape[1]
+
+    assert num_classes == len(
+        labels
+    ), f"y_pred_proba size {num_classes} does not match labels count {len(labels)}"
+
+    label_binarizer = LabelBinarizer().fit(np.arange(num_classes))
+    y_true_ohe = label_binarizer.transform(y_true)
+
+    for class_id, label in enumerate(labels):
+        title = f"Probability distribution '{label}' vs rest"
+
+        if subtitle:
+            title = f"{title} ({subtitle})"
+
+        fig = px.histogram(
+            pd.DataFrame(
+                {
+                    "prob": y_pred_proba[:, class_id],
+                    label: y_true_ohe[:, class_id],
+                }
+            ),
+            x="prob",
+            color=label,
+            color_discrete_map={1: "green", 0: "red"},
+            nbins=50,
+            marginal="box",
+            title=title,
+            barmode="overlay",
+        )
+
+        fig.show()
